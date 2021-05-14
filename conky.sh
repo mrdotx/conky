58485b3740a40dbda85302cce4e62836dd40e58e
mrdotx <klassiker@gmx.de>
2021-05-14 14:44:19 +0200

    structure fix

--- conky.sh
+++ conky.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/conky/conky.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/conky
-# date:   2021-01-15T13:31:18+0100
+# date:   2021-05-14T14:38:39+0200
 
 conky_dir="conky -q -c $HOME/.config/conky"
 
@@ -15,54 +15,54 @@ conky3="$conky_dir/klassiker_vertical.conf"
 choice=${1:-vertical}
 
 case "$choice" in
-vertical)
-    if [ "$(pgrep -f "$conky3")" ]; then
-        kill "$(pgrep -f "$conky3")"
-    else
-        $conky3
-    fi
-    ;;
-slim-horizontal)
-    if [ "$(pgrep -f "$conky2")" ]; then
-        kill "$(pgrep -f "$conky2")"
-    else
-        $conky2
-    fi
-    ;;
-horizontal)
-    if [ "$(pgrep -f "$conky1")" ]; then
-        kill "$(pgrep -f "$conky1")"
-    else
-        $conky1
-    fi
-    ;;
-large)
-    if [ "$(pgrep -f "$conky3")" ]; then
-        # vertical -> stop"
-        kill "$(pgrep -f "$conky3")"
-    elif [ "$(pgrep -f "$conky2")" ]; then
-        # slim horizontal -> vertical"
-        kill "$(pgrep -f "$conky2")" \
-            && $conky3
-    elif [ "$(pgrep -f "$conky1")" ]; then
-        # horizontal -> slim horizontal"
-        kill "$(pgrep -f "$conky1")" \
-            && $conky2
-    else
-        $conky1
-    fi
-    ;;
-small)
-    if [ "$(pgrep -f "$conky2")" ]; then
-        # slim horizontal -> horizontal"
-        kill "$(pgrep -f "$conky2")" \
-            && $conky1
-    elif [ "$(pgrep -f "$conky1")" ]; then
-        # horizontal -> slim horizontal"
-        kill "$(pgrep -f "$conky1")" \
-            && $conky2
-    else
-        $conky1
-    fi
-    ;;
+    vertical)
+        if [ "$(pgrep -f "$conky3")" ]; then
+            kill "$(pgrep -f "$conky3")"
+        else
+            $conky3
+        fi
+        ;;
+    slim-horizontal)
+        if [ "$(pgrep -f "$conky2")" ]; then
+            kill "$(pgrep -f "$conky2")"
+        else
+            $conky2
+        fi
+        ;;
+    horizontal)
+        if [ "$(pgrep -f "$conky1")" ]; then
+            kill "$(pgrep -f "$conky1")"
+        else
+            $conky1
+        fi
+        ;;
+    large)
+        if [ "$(pgrep -f "$conky3")" ]; then
+            # vertical -> stop"
+            kill "$(pgrep -f "$conky3")"
+        elif [ "$(pgrep -f "$conky2")" ]; then
+            # slim horizontal -> vertical"
+            kill "$(pgrep -f "$conky2")" \
+                && $conky3
+        elif [ "$(pgrep -f "$conky1")" ]; then
+            # horizontal -> slim horizontal"
+            kill "$(pgrep -f "$conky1")" \
+                && $conky2
+        else
+            $conky1
+        fi
+        ;;
+    small)
+        if [ "$(pgrep -f "$conky2")" ]; then
+            # slim horizontal -> horizontal"
+            kill "$(pgrep -f "$conky2")" \
+                && $conky1
+        elif [ "$(pgrep -f "$conky1")" ]; then
+            # horizontal -> slim horizontal"
+            kill "$(pgrep -f "$conky1")" \
+                && $conky2
+        else
+            $conky1
+        fi
+        ;;
 esac
